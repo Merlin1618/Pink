156029494aff64e4dce2c17a1578613dc171e6c1
Dmitrij Yanenko <yanenko.dmitrij@mail.ru>
2018-04-05 14:00:13 +0500

    Update

---
+++ js/Main.js
@@ -0,0 +1,1 @@
+<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A37fbf1c790e3edecc8d014eba80ec836b65156a651fc5272ac25857888d4f811&amp;width=100%25&amp;height=553&amp;lang=ru_RU&amp;scroll=true"></script>
